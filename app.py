bc49b399743ea879fd6f3cf3bfeabd68cd9af8b6
Trollicorn <muddin3@stuy.edu>
2019-03-12 15:54:44 -0400

    aaaaaaaa

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import sqlite3
 from string import ascii_letters,digits
 from os import urandom,remove
 from random import randint
-from flask import Flask,render_template,request,flash,redirect,url_for,make_response,Request
+from flask import Flask,render_template,request,flash,redirect,url_for,make_response,Request,session
 app=Flask(__name__)
 app.secret_key=urandom(32)
 @app.route("/")
@@ -13,58 +13,54 @@ def home():
     if "user"in session:
         return redirect("/schkjuul")
     return render_template("home.html")
-@app.route("/schkjuul",methods=["POST"])
-def login():
-    if not "user" in session:
-        if"username"not in request.form:
 @app.route("/schkjuul",methods=["GET","POST"])
 def login():
-    db=sqlite3.connect(url_for("util",filename="users.db"))
+    db=sqlite3.connect("util/users.db")
     squul=db.cursor()
     if not"user"in session:
-        if request.method=="GET"or"username"not in request.form:
+        if request.method=="GET"or"user"not in request.form or"pass"not in request.form:
             return redirect("/")
-        if not logcheck(request.form["username"],request.form["password"],squul):
-            flash("Username or password is the not good.")
+        if not logcheck(request.form["user"],request.form["pass"],squul):
+            flash("User or pass is the not good.")
             return redirect("/")
-        session["user"]=request.form["username"]
+        session["user"]=request.form["user"]
     try:
         sl=int(request.form["slide_location"])
     except TypeError:
         sl=0
-    return render_template("sql.html", sqltable=usersql("username"), userinf=userinf(session["user"]),slide_location=sl)
+    return render_template("sql.html", sqltable=usersql("user"), userinf=userinf(session["user"]),slide_location=sl)
     tmp=userinf(session["user"],squul)
     db.close()
-    return render_template("sql.html",sqltable=usersql("username"),userinf=tmp)
+    return render_template("sql.html",sqltable=usersql("user"),userinf=tmp)
 @app.route("/registerr",methods=["GET","POST"])
 def registerr():
-    if request.method=="GET"or"username"not in request.form or"password"not in request.form:
+    if request.method=="GET"or"user"not in request.form or"pass"not in request.form:
         return redirect("/")
-    if"users"==request.form["username"]or not all(i in(ascii_letters+digits)for i in request.form["username"]):
-        flash("Username must be strictly alphanumeric.")
+    if"users"==request.form["user"]or not all(i in(ascii_letters+digits)for i in request.form["user"]):
+        flash("User must be strictly alphanumeric.")
         return redirect("/")
-    db=sqlite3.connect(url_for("util",filename="users.db"))
+    db=sqlite3.connect("util/users.db")
     squul=db.cursor()
-    squul.execute("INSERT INTO users VALUES(?, ?, ?);",(squul.execute("SELECT COUNT(*) FROM users;").fetchone[0],request.form["username"],request.form["password"]))
+    squul.execute("INSERT INTO users VALUES(?, ?, ?);",(squul.execute("SELECT COUNT(*) FROM users;").fetchone[0],request.form["user"],request.form["pass"]))
     squul.commit()
     db.close()
-    usersqlify(request.form["username"])
+    usersqlify(request.form["user"])
     return redirect("/schkjuul")
 def logcheck(u,p,squul):
-    return p==squul.execute("SELECT password FROM users WHERE username = ?;",(u,)).fetchone()[0]
+    return p==squul.execute("SELECT pass FROM users WHERE user = ?;",(u,)).fetchone()[0]
 def userinf(u,squul):
-    return squul.execute("SELECT * FROM users WHERE username = ?;",(u,)).fetchone()
+    return squul.execute("SELECT * FROM users WHERE user = ?;",(u,)).fetchone()
 def usersql(u):
-    nudeb=sqlite3.connect(url_for("util/",filename=u+".db"))
+    nudeb=sqlite3.connect("util/"+u+".db")
     nusquul=nudeb.cursor()
     b=nusquul.execute("SELECT * FROM sql;").fetchall()
     nudeb.close()
     return b
 def usersqlify(u):
-    remove(url_for("util",filename=u+".db"))
-    nudeb=sqlite3.connect(url_for("util/",filename=u+".db"))
+    remove("util/"+u+".db")
+    nudeb=sqlite3.connect("util/"+u+".db")
     nusquul=nudeb.cursor()
-    nusquul.execute("CREATE TABLE users (userid INTEGER PRIMARY KEY, username TEXT, password TEXT);")
+    nusquul.execute("CREATE TABLE users (userid INTEGER PRIMARY KEY, user TEXT, pass TEXT);")
     nusquul.execute("INSERT INTO users VALUES(?, ?, ?);",(0,"Mohammed Uddin","Zbunzzrq Hqqva"))
     nusquul.execute("INSERT INTO users VALUES(?, ?, ?);",(1,"AzureLobster","NmherYbofgre"))
     nusquul.execute("INSERT INTO users VALUES(?, ?, ?);",(2,"BidOOF","OvqBBS"))
@@ -72,14 +68,18 @@ def usersqlify(u):
     nusquul.commit()
     nudeb.close()
 def reset():
-    db=sqlite3.connect(url_for("util",filename="users.db"))
+    db=sqlite3.connect("util/users.db")
     squul=db.cursor()
-    for i in squul.execute("SELECT username FROM users;").fetchall():
-        remove(url_for("util",filename=i[0]+".db"))
+    try:
+        for i in squul.execute("SELECT user FROM IF EXISTS users;").fetchall():
+            remove("util/"+i[0]+".db")
+    except sqlite3.OperationalError:
+        pass
     squul.execute("DROP TABLE IF EXISTS users;")
-    squul.execute("CREATE TABLE users (userid INTEGER PRIMARY KEY, username TEXT, password TEXT);")
-    squul.commit()
+    squul.execute("CREATE TABLE users (userid INTEGER PRIMARY KEY, user TEXT, pass TEXT);")
+    db.commit()
     db.close()
 if __name__=="__main__":
+    reset()
     app.debug=True
     app.run()
