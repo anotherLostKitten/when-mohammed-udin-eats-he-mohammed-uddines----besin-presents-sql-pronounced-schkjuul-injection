7d19cb21ff6ea7ddd55e7f5620f47b0dbb4ea6fc
Theodore Peters <tpeters@stuy.edu>
2019-03-14 02:18:43 -0400

    very pretty

--- static/doesitmatter.js
+++ static/doesitmatter.js
@@ -1,16 +1,20 @@
 var formy=document.getElementById("loggy");
+var register = document.getElementById("registery");
+var login = document.getElementById("loginry");
 
 var switcherooR=(e)=>{
-  formy.innerHTML="<h4>Register:</h4><form action=\"/registerr\" method=\"POST\"><input type=\"text\"  name=\"user\" placeholder=\"username\"><input type=\"text\"  name=\"pass\" placeholder=\"password\"><input type=\"submit\" name=\"regi\" value=\"Register\"></form>";
+	formy.innerHTML="<h4>Register:</h4><br><form action=\"/registerr\" method=\"POST\"><input type=\"text\" class=\"intxt\"  name=\"user\" placeholder=\"Username\"><br><br><input class=\"intxt\" type=\"text\"  name=\"pass\" placeholder=\"Password\"><br><br><input type=\"submit\" name=\"regi\" value=\"Register\" class=\"btn btn-primary\"></form>";
+	register.className = "btn btn-light";
+	login.className = "btn btn-info";
 };
 
 var switcherooL=(e)=>{
-  formy.innerHTML="<h4>Login:</h4>  <form action=\"/schkjuul\" method=\"POST\"> <input type=\"text\" name=\"user\" placeholder=\"username\"> <input type=\"text\"  name=\"pass\" placeholder=\"password\">  <input type=\"submit\" name=\"auth\" value=\"Log In\"></form>";
+	formy.innerHTML="<h4>Login:</h4><br><form action=\"/schkjuul\" method=\"POST\"> <input class=\"intxt\" type=\"text\" name=\"user\" placeholder=\"Username\"><br><br><input class=\"intxt\"  type=\"text\"  name=\"pass\" placeholder=\"Password\"><br><br><input type=\"submit\" name=\"auth\" value=\"Log In\" class=\"btn btn-primary\"></form>";
+	register.className = "btn btn-info";
+	login.className = "btn btn-light";
 };
 
 
-var register = document.getElementById("registery");
 register.addEventListener('click',switcherooR);
 
-var login = document.getElementById("loginry");
 login.addEventListener('click',switcherooL);
